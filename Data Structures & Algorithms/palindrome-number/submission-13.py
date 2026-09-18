89a1e060a3f1cb3f0fdda7e0c028587887cd64c6
arielhalevy123 <130355480+arielhalevy123@users.noreply.github.com>
2026-09-18 17:41:19 +0300

    Add: palindrome-number - submission-13

---
+++ Data Structures & Algorithms/palindrome-number/submission-13.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        dup=x
+        mult=1
+        if x<0:
+            return False
+        while dup !=0:
+            dup=dup//10 
+            mult =mult*10
+        mult=mult//10
+        dev=10
+        while mult >= dev // 10:
+            if x%dev//(dev//10)!=(x//mult)%10:
+                return False
+            mult=mult//10
+            dev=dev*10
+        return True
+            
+
+
+        
