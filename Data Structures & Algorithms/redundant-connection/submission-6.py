3ae801f4811e4925244aad6971c592d6ca01b986
arielhalevy123 <130355480+arielhalevy123@users.noreply.github.com>
2026-09-26 13:01:37 +0300

    Add: redundant-connection - submission-6

---
+++ Data Structures & Algorithms/redundant-connection/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        par =[i for i in range(len(edges)+1)]
+        rank=[1]*(len(edges)+1)
+    
+        def find(n):
+            
+            while n != par[n]:
+                n=par[n]
+            return n
+        def union(n1,n2):
+            par1,par2=find(n1),find(n2)
+            if par1==par2:
+                return False
+            if rank[par1]>rank[par2]:
+                par[par2]=par1
+                rank[par1]+=1
+            else:
+                par[par1]=par2
+                rank[par2]+=1
+            return True
+        for n1,n2 in edges:
+            if not union(n1,n2):
+                return [n1,n2]
+            
+        return []
+
+        
